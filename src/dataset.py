428c9443bbb852c6aff595d999d47a30c6adc6d0
Miki Yutani <mkyutani@gmail.com>
2022-01-10 18:46:12 +0900

    Generate all-in-one json from open data site

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import csv
+import hashlib
 import json
 import os
 import re
@@ -9,21 +10,34 @@ import sys
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 
-def csv2json(url, has_header=False, trial_encoding=None):
+def create_dataset(dataset_profile, trial_encoding=None):
+
+    id = dataset_profile['id']
+    name = dataset_profile['name']
+    url = dataset_profile['url']
+
+    content = {
+        'meta': {
+            'id': id,
+            'url': url,
+            'name': name
+        },
+        'data': {}
+    }
 
     res = None
     try:
         res = requests.get(url)
     except requests.exceptions.RequestException as e:
-        print(f'failed to fetch {url}', file=sys.stderr)
-        return None
+        print(f'Failed to fetch {url}', file=sys.stderr)
+        return content
     except Exception as e:
-        print(f'failed to fetch {url}', file=sys.stderr)
-        return None
+        print(f'Failed to fetch {url}', file=sys.stderr)
+        return content
 
     if res.status_code >= 400:
-        print(f'failed to fetch {url}. Status code={res.status_code}', file=sys.stderr)
-        return None
+        print(f'Failed to fetch {url}. Status code={res.status_code}', file=sys.stderr)
+        return content
 
     reason = None
     text = None
@@ -40,11 +54,11 @@ def csv2json(url, has_header=False, trial_encoding=None):
 
     if text is None:
         if trial_encoding is None:
-            print(f'failed to decode {url}.', file=sys.stderr)
+            print(f'Failed to decode {url}.', file=sys.stderr)
             print(f'  tried encoding: {enc}. Reason={reason}', file=sys.stderr)
             print(f'  requests.encoding: {res.encoding}', file=sys.stderr)
             print(f'  requests.apparent_encoding: {res.apparent_encoding}', file=sys.stderr)
-            return None
+            return content
         else:
             reason2 = None
             enc2 = trial_encoding
@@ -52,66 +66,65 @@ def csv2json(url, has_header=False, trial_encoding=None):
                 text = res.content.decode(enc2)
             except Exception as e:
                 reason2 = e
-                print(f'failed to decode {url}.', file=sys.stderr)
+                print(f'Failed to decode {url}.', file=sys.stderr)
                 print(f'  tried encoding: {enc}. Reason={reason}', file=sys.stderr)
                 print(f'  tried encoding: {enc2}. Reason={reason2}', file=sys.stderr)
                 print(f'  requests.encoding: {res.encoding}', file=sys.stderr)
                 print(f'  requests.apparent_encoding: {res.apparent_encoding}', file=sys.stderr)
-                return None
+                return content
 
     rows = csv.reader(text.splitlines())
-    meta = {}
     data = {}
-    if has_header:
-        lno = -1
-    else:
-        lno = 0
+    lno = 0
     for row in rows:
         cno = 0
-        if lno < 0:
-            header = {}
-            for column in row:
-                header[str(cno)] = column
-                cno = cno + 1
-            meta['header'] = header
-        else:
-            entity = {}
-            empty = True
-            for column in row:
-                entity[str(cno)] = column
-                if len(column) > 0:
-                    empty = False
-                cno = cno + 1
-            if empty is False:
-                data[str(lno)] = entity
+        entity = {}
+        empty = True
+        for column in row:
+            entity[str(cno)] = column
+            if len(column) > 0:
+                empty = False
+            cno = cno + 1
+        if empty is False:
+            data[str(lno)] = entity
         lno = lno + 1
-    content = {
-        'meta': meta,
-        'data': data
-    }
 
-    return json.dumps(content, ensure_ascii=False, indent=1)
-                
-def list_csvs(url):
+    print(f'Read {lno} data from dataset in {url}.')
+
+    content['data'] = data
+
+    return content
+
+def generate_dataset_name(url):
+    parsed = urlparse(url)
+    netloc_hierarchy = parsed.netloc.split('.')
+    netloc_hierarchy.reverse()
+    path_hierarchy = parsed.path.split('/')
+    path_hierarchy.pop(0)
+    path_hierarchy[-1] = re.sub('.csv$', '', path_hierarchy[-1])
+    name = '.'.join(netloc_hierarchy) + '.' + '.'.join(path_hierarchy)
+    return name
+
+def create_dataset_profile_list(url):
 
     res = None
     try:
         res = requests.get(url)
     except requests.exceptions.RequestException as e:
-        print(f'failed to fetch {url}', file=sys.stderr)
+        print(f'Failed to fetch {url}', file=sys.stderr)
         return None
     except Exception as e:
-        print(f'failed to fetch {url}', file=sys.stderr)
+        print(f'Failed to fetch {url}', file=sys.stderr)
         return None
 
     if res.status_code >= 400:
-        print(f'failed to fetch {url}. Status code={res.status_code}', file=sys.stderr)
+        print(f'Failed to fetch {url}. Status code={res.status_code}', file=sys.stderr)
         return None
 
     url_object = urlparse(url)
     url_netloc = url_object.netloc.lower()
 
-    selected = []
+    dataset_profile_list = []
 
     bs = BeautifulSoup(res.content, 'html.parser')
     for t in bs.find_all('a'):
@@ -129,36 +142,44 @@ def list_csvs(url):
             path = ref_object.path.lower()
             if scheme == 'http' or scheme == 'https':
                 if netloc == url_netloc and path.endswith('.csv'):
-                    selected.append(ref_full)
+                    dataset_url = ref_full
+                    dataset_name = generate_dataset_name(dataset_url)
+                    dataset_id = hashlib.md5(dataset_name.encode()).hexdigest()
+                    dataset_profile_list.append({
+                        'id': dataset_id,
+                        'name': dataset_name,
+                        'url': dataset_url
+                    })
+                    print(f'Found {dataset_name}', file=sys.stderr)
 
-    return selected
+    print(f'Generated {len(dataset_profile_list)} dataset set.', file=sys.stderr)
 
-def create_dataset(url, dir=None, has_header=False, trial_encoding=None):
+    return dataset_profile_list
+
+def generate(url, dir=None, trial_encoding=None):
+
+    dataset_profile_list = create_dataset_profile_list(url)
+    ds_all = {}
+    for dsp in dataset_profile_list:
+        ds = create_dataset(dsp, trial_encoding=trial_encoding)
+        ds_all.update({
+            dsp['id']: ds
+        })
 
     if dir == None:
         dir = ''
     else:
         os.makedirs(dir, mode=0o777, exist_ok=True)
-        if not dir.endswith('/'):
-            dir = dir + '/'
-
-    csvs = list_csvs(url)
-    for c in csvs:
-        json = csv2json(c, has_header=has_header, trial_encoding=trial_encoding)
-        if json is not None:
-            fn = re.sub('/', '--', urlparse(c).path)
-            fn = re.sub('^--', '', fn)
-            fn = re.sub('.csv$', '', fn)
-            fn = fn + '.json'
-            fpath = dir + fn
-            with open(fpath, 'w') as fd:
-                fd.write(json)
+    dump = json.dumps(ds_all, ensure_ascii=False, indent=1)
+    fpath = os.path.join(dir, 'datasets.json')
+    with open(fpath, 'w') as fd:
+        fd.write(dump)
 
 if __name__ == '__main__':
 
     def usage():
         print('parameters: method url ...', file=sys.stderr)
-        print('  method: create_dataset, csv2json, list_csvs', file=sys.stderr)
+        print('  method: create_dataset', file=sys.stderr)
 
     if len(sys.argv) < 3:
         usage()
@@ -166,26 +187,12 @@ if __name__ == '__main__':
 
     method = sys.argv[1]
     url = sys.argv[2]
-    if method == 'create_dataset':
+    if method == 'generate':
         if len(sys.argv) > 3:
             dir = sys.argv[3]
         else:
             dir = None
-        has_header = False
-        if len(sys.argv) > 4 and sys.argv[4].lower() == 'true':
-            has_header = True
-        create_dataset(url, dir=dir, has_header=has_header, trial_encoding='cp932')
-    elif method == 'csv2json':
-        has_header = False
-        if len(sys.argv) > 3 and sys.argv[3].lower() == 'true':
-            has_header = True
-        json = csv2json(url, has_header=has_header, trial_encoding='cp932')
-        print(json)
-    elif method == 'list_csvs':
-        csvs = list_csvs(url)
-        for c in csvs:
-            print(c)
-        print(len(csvs))
+        generate(url, dir=dir, trial_encoding='cp932')
     else:
         usage()
         exit(1)
